98ee4fb3a4462a10c4e68909324ab1665a73ecac
Arthur Kraus <akraus3@luc.edu>
2025-04-21 10:50:02 -0500

    id hits

--- cluster.cpp
+++ cluster.cpp
@@ -1,91 +1,96 @@
 #include <iostream>
+//#include <queue>
+#include "cluster.h"
 using namespace std;
 
-// allows compiler to know array size
-//
 // TODO: HLS has fixed width floating-point types
 // 		definitely need that for the centroid calculations
  
+// struct Point2D {
+// 	int x;
+// 	int y;
+// 	
+// 	Point2D(int x, int y) {
+// 		this->x = x;
+// 		this->y = y;
+// 	}
+// }
+
+
+// allows compiler to know array size
 template<size_t Rows, size_t Cols>
+
+// pair<double, double> calcCentroid() {
+// 	// calc centroid	
+// }
+
 
 /* sw_findMaxSum Parameters: 
  * 	&arr - reference to 2D array, not decayed to pointer (loses size info)
  * 	k->length of submatrices (for 10x10, make it 5)
  */
 int sw_findMaxSum(int (&arr)[Rows][Cols], int k) { 
-	// init variables 
 	int maxSum = 0;
-	int sum1 = 0;
-	int sum2 = 0;
-	
-	// error checking to see if k = {Rows,Cols}/2
-	if ( (Rows/2 != k) || (Cols/2 != k) ) {
-		cout << "Rows = " << Rows
-		     << "Cols = " << Cols
-		     << "k = " << k 
-		     << endl;
-	}	
-	
-	// Determine the starting positions of each window
-	// first pos. 
-	int x1 = 0; // 0->k
-	int y1 = 0; // 0->k	
-	//cout << "first pos is {"<<x1<<","<<y1<<"}"<<endl; 		
-	// second pos. 
-	int x2 = (Cols-k);// (Cols-k) -> Cols
-	int y2 = 0; // 0 -> k	
-	//cout << "second pos is {"<<x2<<","<<y2<<"}"<<endl; 			
-	//third pos.
-	int x3 = 0; // 0 -> k 
-	int y3 = (Rows-k); // (Rows - k) -> Rows 
-	//cout << "third pos is {"<<x3<<","<<y3<<"}"<<endl; 			
-	//fourth pos.
-	int x4 = (Cols-k); // (Cols-k) -> Cols = x2 
-	int y4 = (Rows-k); // (Rows - k) -> Rows 
-	//cout << "fourth pos is {"<<x4<<","<<y4<<"}"<<endl; 			
 
-	// == Stg1 ==
-	// apply the window on the first k rows
-	for(int i = x1; i < k; i++) {
+	// array of the 8-adjacent spaces for each direction
+	int drw[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+	int dcl[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+
+	//TODO: queue of cluster points
+		
+	for(int i = 0; i < k; i++) { // Rows at x=0
 		// calculate values on the first k cols
-		for(int j = y1; j < k; j++) {
-			sum1+=arr[i][j];
-			cout << "Stg1; sum1 = " << sum1 << endl; // print_debug
+		for(int j = 0; j < k; j++) { // Cols at y=0
+			// check if the value is a 1
+			if(arr[i][j] == 1) { // it's a hit!
+				cout << "win1: arr["<<i<<"]["<<j<<"] HIT!"<<endl;
+
+				// determine if cluster
+					// loop over all directions	
+					// add to cluster
+			} 
 		}
-		// compare to find the max sum as we go, and store the max value
-		maxSum = max(maxSum,sum1);
 
 		//moving the window one column to the right and calculating values of next
-		for(int j = x2; j<Cols; j++) {
-			sum2 += arr[i][j];
-			cout << "Stg1; sum2 = " << sum2 << endl; // print_debug
+		for(int j = Cols-k; j<Cols; j++) {
+			// check if the value is a 1
+			if(arr[i][j] == 1) { // it's a hit!
+				cout << "win2: arr["<<i<<"]["<<j<<"] HIT!"<<endl;
+
+				// determine if cluster
+					// loop over all directions	
+					// add to cluster
+			}
 		}
-		maxSum = max(maxSum,sum2);
 	}	
-	// resetting vars for comparison
-	sum1=0; 
-	sum2=0;
 	
-	// == Stg2 ==
-	// moving window down 
-	
-	for(int i = x3; i < Rows; i++) {
-		// calculating values of first two columns
-		for(int j = y3; j < k; j++) {
-			sum1 += arr[i][j];
-			cout << "Stg2; sum1 = " << sum1 << endl; // print_debug
+	for(int i = Rows-k; i < Rows; i++) { // Rows at x=Rows-k
+		for(int j = 0; j < k; j++) {
+			// check if the value is a 1
+			if(arr[i][j] == 1) { // it's a hit!
+				cout << "win3: arr["<<i<<"]["<<j<<"] HIT!"<<endl;
+
+				// determine if cluster
+					// loop over all directions	
+					// add to cluster
+			}
 		}
-		maxSum = max(maxSum,sum1);
-		for(int j = k-1; j < Cols; j++) {
-			sum2 += arr[i][j];
-			cout << "Stg2; sum2 = " << sum2 << endl; // print_debug
+		for(int j = Cols-k; j < Cols; j++) {
+			// check if the value is a 1
+			if(arr[i][j] == 1) { // it's a hit!
+				cout << "win4: arr["<<i<<"]["<<j<<"] HIT!"<<endl;
+
+				// determine if cluster
+					// loop over all directions	
+					// add to cluster
+			}
+
 		}
-		// comparing the sum2 with the last max sum found
-		maxSum = max(maxSum, sum2);
 	}
-	// returning the maximum sum value
 	return maxSum;
 } 
+
+
 
 //void writeClusterPointsCSV(const std::vector<Point>& points, const std::string& filename) {
 //	std::ofstream outfile(filename);
@@ -116,21 +121,6 @@ int sw_findMaxSum(int (&arr)[Rows][Cols], int k) {
 //}
 
 int main() {
-	const int arr_size = 10;	
-	const int k = 5;
-
-	int sample_arr[arr_size][arr_size] = {
-   	    {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-   	    {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-   	    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-   	    {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-   	    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-   	    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   	    {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
-   	    {0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-   	    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-   	};
 
 	sw_findMaxSum(sample_arr, k);
 
@@ -141,7 +131,7 @@ int main() {
 	// 		(TODO: make sure diagonal hits are accounted for!!!!)
       
 	// TODO: Centroid calculations!
-	
+		
 
         // Write output files
         // writeClusterPointsCSV(points, "cluster_points.csv");
