11013fb6fa9bb23c74ccbe279b3c4ecc487f6c67
Nicholas Barnett <133896112+thenicholasbarnett@users.noreply.github.com>
2025-04-23 21:53:28 -0500

    Add files via upload

---
+++ Cluster_Generation_2.h
@@ -0,0 +1,190 @@
+// defining parameters of grid and cluster sizes //
+
+// decided grid number and size
+const int nrow = 10;
+const int ncol = 10;
+// const int nrow = 100;
+// const int ncol = 100;
+
+// biggest hit number for a cluster
+const int nhitmax = 9;
+const int hitmaxval = 9;
+
+// max number of clusters in grid
+const int nclustmax = 9;
+// const int nclustmax = 50;
+
+// making new grid
+int grid[nrow][ncol] = {0};
+
+// inserting cluster into grid
+void insert_cluster(int g[nrow][ncol]){
+    // picking a random point on the grid for inital point of cluster and placing it on the grid
+    int x = rand() % nrow;
+    int y = rand() % ncol;
+
+    // placing the initial hit of the cluster into the g
+    int hitval1 = rand() % hitmaxval;
+    g[x][y] = hitval1;
+    // cout<<"hitval1 is "<<hitval1<<endl;
+
+    // making a random number of max hits in cluster
+    int hitnum = rand() % nhitmax;
+
+    // randomly orienting the other hits around the initial hit
+    for(unsigned int i=0; i<hitnum; i++){
+        if(hitval1==0){continue;}
+        
+        // randomly orienting the hits around the initial point in one of eight positions
+        // 0 1 2
+        // 3 x 4
+        // 5 6 7
+        // where x is the initial hit
+        int hitloc = rand() % 8;
+
+        // // printing the next hit in the cluster's location 
+        // cout<<endl<<hitloc<<endl;
+
+        // skipping the suggested random hit in the boundary cases
+        if((x==0)&&((hitloc==0)||(hitloc==1)||(hitloc==2))){continue;}
+        if((x==nrow-1)&&((hitloc==5)||(hitloc==6)||(hitloc==7))){continue;}
+        if((y==0)&&((hitloc==0)||(hitloc==3)||(hitloc==5))){continue;}
+        if((y==ncol-1)&&((hitloc==2)||(hitloc==4)||(hitloc==7))){continue;}
+
+        // placing the next hit in the cluster onto the grid
+        if(hitloc == 0){
+            int x_h=x-1;
+            int y_h=y-1;
+            int hitval = rand() % hitval1;
+            // cout<<"hitval is "<<hitval<<endl;
+            g[x_h][y_h]=hitval;
+        }
+        if(hitloc == 1){
+            int x_h=x-1;
+            int hitval = rand() % hitval1;
+            // cout<<"hitval is "<<hitval<<endl;
+            g[x_h][y]=hitval;
+        }
+        if(hitloc == 2){
+            int x_h=x-1;
+            int y_h=y+1;
+            int hitval = rand() % hitval1;
+            // cout<<"hitval is "<<hitval<<endl;
+            g[x_h][y_h]=hitval;
+        }
+        if(hitloc == 3){
+            int y_h=y-1;
+            int hitval = rand() % hitval1;
+            // cout<<"hitval is "<<hitval<<endl;
+            g[x][y_h]=hitval;
+        }
+        if(hitloc == 4){
+            int y_h=y+1;
+            int hitval = rand() % hitval1;
+            // cout<<"hitval is "<<hitval<<endl;
+            g[x][y_h]=hitval;
+        }
+        if(hitloc == 5){
+            int x_h=x+1;
+            int y_h=y-1;
+            int hitval = rand() % hitval1;
+            // cout<<"hitval is "<<hitval<<endl;
+            g[x_h][y_h]=hitval;
+        }
+        if(hitloc == 6){
+            int x_h=x+1;
+            int hitval = rand() % hitval1;
+            // cout<<"hitval is "<<hitval<<endl;
+            g[x_h][y]=hitval;
+        }
+        if(hitloc == 7){
+            int x_h=x+1;
+            int y_h=y+1;
+            int hitval = rand() % hitval1;
+            // cout<<"hitval is "<<hitval<<endl;
+            g[x_h][y_h]=hitval;
+        }
+    }
+}
+
+// overlap check
+void overlap_check(int f, int g[nrow][ncol], int tg[nrow][ncol]){
+    for(unsigned int i=0; i<nrow; i++){
+        for(unsigned int j=0; j<ncol; j++){
+
+            // checking if hit overlaps
+            if(tg[i][j]==0){continue;}
+            if(g[i][j]!=0){f=1;}
+
+            // checking if adjacent there are adjacent hits to cluster in temp grid
+            for(unsigned int b=0;b<8;b++){
+
+                // skipping boundary cases
+                if((i==0)&&((b==0)||(b==1)||(b==2))){continue;}
+                if((i==nrow-1)&&((b==5)||(b==6)||(b==7))){continue;}
+                if((j==0)&&((b==0)||(b==3)||(b==5))){continue;}
+                if((j==ncol-1)&&((b==2)||(b==4)||(b==7))){continue;}
+
+                if(b==0){
+                    int c0=i-1;
+                    int c1=j-1;
+                    if(g[c0][c1]!=0){f=1;}
+                }
+                if(b==1){
+                    int c0=i-1;
+                    if(g[c0][j]!=0){f=1;}
+                }
+                if(b==2){
+                    int c0=i-1;
+                    int c1=j+1;
+                    if(g[c0][c1]!=0){f=1;}
+                }
+                if(b==3){
+                    int c1=j-1;
+                    if(g[i][c1]!=0){f=1;}
+                }
+                if(b==4){
+                    int c1=j+1;
+                    if(g[i][c1]!=0){f=1;}
+                }
+                if(b==5){
+                    int c0=i+1;
+                    int c1=j-1;
+                    if(g[c0][c1]!=0){f=1;}
+                }
+                if(b==6){
+                    int c0=i+1;
+                    if(g[c0][j]!=0){f=1;}
+                }
+                if(b==7){
+                    int c0=i+1;
+                    int c1=j+1;
+                    if(g[c0][c1]!=0){f=1;}
+                }
+            }
+        }
+    }
+
+    // printing the temporary grid
+    // if(f==1){cout<<"the following cluster grid has overlap with the current grid"<<endl;}
+    // cout<<"cluster grid"<<endl;
+    // for(unsigned int i=0; i<nrow; i++){
+    //     for(unsigned int j=0; j<ncol; j++){
+    //         cout<<tg[i][j];
+    //     }
+    //     cout<<endl;
+    // }
+    // cout<<endl;
+
+    // mapping the separate grid and cluster to the original grid if there's no overlap between the two
+    if(f==0){
+        for(unsigned int i=0; i<nrow; i++){
+            for(unsigned int j=0; j<ncol; j++){
+                if(tg[i][j]!=0){
+                    g[i][j]=tg[i][j];
+                    // cout<<"element in temp grid is "<<tg[i][j]<<endl;
+                }
+            }
+        }
+    }
+}
